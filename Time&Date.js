a937871de621150bf7fb40f21f1f05455a0f5a38
Rupak Ghosh <rupakghosh10101@gmail.com>
2025-01-24 06:05:47 +0000

    date and time commit

---
+++ Time&Date.js
@@ -0,0 +1,14 @@
+// console.log(Math.floor(new Date().getTime()/1000))
+// let hour = new Date().getHours()
+// let min = new Date().getMinutes()
+// let sec = new Date().getSeconds()
+// console.log(`${hour}:${min}:${sec}`)
+
+//  console.log(new Date().toLocaleString()) 1/23/2025, 11:15:09 PM
+// console.log(new Date().toLocaleDateString())  1/23/2025
+// console.log(new Date().toISOString())    //2025-01-23T17:29:35.884Z
+// console.log(new Date().toJSON())        //2025-01-23T17:30:40.080Z
+// console.log(new Date().toString())   Thu Jan 23 2025 23:17:09 GMT+0530 (India Standard Time)
+// console.log(new Date().getTime())
+
+// console.log( new Date().toLocaleDateString())
---
+++ string.js
@@ -0,0 +1,14 @@
+const bgc = new String("Rupak $% Ghosh . com")
+// console.log(bgc);
+// console.log(bgc.bold());
+// console.log(bgc.charAt(2));
+// console.log(bgc.endsWith("k?"))
+// console.log(bgc.indexOf("u"));
+// console.log(bgc.length);
+
+console.log(bgc.slice(2,6));
+console.log(bgc.split(" "))
+
+console.log(bgc.replace("$%","rt"))
+
+console.log(bgc.trim())
